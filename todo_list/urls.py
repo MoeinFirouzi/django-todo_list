c9862fac98cba202b7a0725f35db7b2ec2b929c9
MoeinFirouzi <firouzi.m.moein@gmail.com>
2021-07-02 23:50:02 +0430

    add cross off & uncross

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -3,4 +3,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('about/', views.about, name="about"),
+    path('delete/<item_id>', views.delete, name="delete"),
+    path('uncross/<item_id>', views.uncross, name="uncross"),
+    path('crossoff/<item_id>', views.cross_off, name="cross_off")
 ]
--- todo_list/views.py
+++ todo_list/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import List
 from .forms import ListForm
 from django.contrib import messages
-
+from django.http import HttpResponseRedirect
 def home(request):
 
     if request.method == "POST":
@@ -17,3 +17,21 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html', {})
+
+def delete(request,item_id):
+    item = List.objects.get(pk=item_id)
+    item.delete()
+    messages.success(request,"Item has been deleted")
+    return redirect('home')
+
+def uncross(request,item_id):
+    item = List.objects.get(pk=item_id)
+    item.completed = False
+    item.save()
+    return redirect('home')
+
+def cross_off(request,item_id):
+    item = List.objects.get(pk=item_id)
+    item.completed = True
+    item.save()
+    return redirect('home')    
