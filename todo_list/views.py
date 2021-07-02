1ac94e7a5e18f5fdeacf52fc3c6ff374be7e2e21
MoeinFirouzi <firouzi.m.moein@gmail.com>
2021-07-02 19:28:47 +0430

    creade model form for items and add message box

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
+from .models import List
+from .forms import ListForm
+from django.contrib import messages
 
 def home(request):
-    #code
-    return render(request, 'home.html',{})
+
+    if request.method == "POST":
+        form = ListForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,"Item has been added to the list")
+
+    all_items = List.objects.all()
+    context = {"items" : all_items}        
+    return render(request, 'home.html', context)
 
 def about(request):
     return render(request, 'about.html', {})
-    
